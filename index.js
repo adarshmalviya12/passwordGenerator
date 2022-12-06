d07353ef02c6009b859d4fd650fb383fe4653922
Adarsh Malviya <adarshmalviya1199@gmail.com>
2022-12-07 00:30:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+
+
+    let passwordOne = document.getElementById("passOne");
+    let passwordTwo = document.getElementById("passTwo");
+
+function randomNo(){
+    let no = Math.floor(Math.random()  * characters.length );
+    return no;
+}
+
+function randomPassword(){
+    let pass1 = "";
+    let pass2 = "";
+
+    for(let i=0;i<15;i++){
+        pass1 += characters[randomNo()];
+        pass2 += characters[randomNo()];
+    }
+    passwordOne.textContent = pass1;
+    passwordTwo.textContent = pass2;
+}
+
+
